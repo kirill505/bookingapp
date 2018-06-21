c178f733c0c0bb447ed919852774d9383a9bf122
Kirill505 <kl.bazarov@gmail.com>
2018-06-21 09:30:08 +0800

    Edited list location at card hotel

--- hotels/models.py
+++ hotels/models.py
@@ -1,11 +1,30 @@
 from django.db import models
+from django.contrib import admin
 from django.utils import timezone
 
 class Hotel(models.Model):
     owner = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     text = models.TextField()
-    location = models.TextField()
+    location = models.CharField(
+        max_length = 100,
+        choices = (
+            ('l01', 'Максимиха'),
+            ('l02', 'Горячинск'),
+            ('l03', 'Энхалук'),
+            ('l03', 'Аршан'),
+        ),
+        default = 'l01'
+    )
+    city = models.CharField(
+        max_length = 100,
+        choices = (
+            ('c01', 'Байкал'),
+            ('c02', 'Аршан'),
+            ('c03', 'Белокурихка'),
+        ),
+        default = 'c01'
+    )
     pre_image = models.FileField()
     price = models.IntegerField()
     created_date = models.DateTimeField(
